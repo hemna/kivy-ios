3e4cee70a7609f59133cc7dfb8b71a86e5b45a4e
Hemna <waboring@hemna.com>
2024-12-05 17:27:19 -0500

    Added kivyMD (from github)

---
+++ kivy_ios/recipes/kivymd/__init__.py
@@ -0,0 +1,25 @@
+# pure-python package, this can be removed when we'll support any python package
+from kivy_ios.toolchain import PythonRecipe, shprint
+from kivy_ios.context_managers import cd
+from os.path import join
+import sh
+
+
+class KivyMDRecipe(PythonRecipe):
+    version = "2.0.0"
+    url = "https://github.com/kivymd/KivyMD/archive/master.zip"
+    depends = ["python"]
+
+    def install(self):
+        plat = list(self.platforms_to_build)[0]
+        build_dir = self.get_build_dir(plat)
+        hostpython_pip = sh.Command(join(self.ctx.dist_dir, "hostpython3", "bin", "pip3"))
+        build_env = plat.get_env()
+        dest_dir = join(self.ctx.dist_dir, "root", "python3")
+        build_env['PYTHONPATH'] = self.ctx.site_packages_dir
+        with cd(build_dir):
+            shprint(hostpython_pip, "install", build_dir, "--prefix", dest_dir, _env=build_env)
+
+
+
+recipe = KivyMDRecipe()
