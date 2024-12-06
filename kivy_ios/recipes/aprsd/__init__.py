6ac8b0117c19148f6acfdbec3b2845f87a23e30a
Hemna <waboring@hemna.com>
2024-12-05 22:15:16 -0500

    Added APRSD

---
+++ kivy_ios/recipes/aprsd/__init__.py
@@ -0,0 +1,25 @@
+# pure-python package, this can be removed when we'll support any python package
+from kivy_ios.toolchain import PythonRecipe, shprint
+from os.path import join
+import sh
+import os
+
+
+class APRSDRecipe(PythonRecipe):
+    version = "master"
+    url = "https://github.com/craigerl/aprsd/archive/master.zip"
+
+    depends = ["python"]
+
+    def install(self):
+        plat = list(self.platforms_to_build)[0]
+        build_dir = self.get_build_dir(plat)
+        os.chdir(build_dir)
+        hostpython = sh.Command(self.ctx.hostpython)
+        build_env = plat.get_env()
+        dest_dir = join(self.ctx.dist_dir, "root", "python3")
+        build_env['PYTHONPATH'] = self.ctx.site_packages_dir
+        shprint(hostpython, "setup.py", "install", "--prefix", dest_dir, _env=build_env)
+
+
+recipe = APRSDRecipe()
